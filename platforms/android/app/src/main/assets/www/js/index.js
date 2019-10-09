c9a9ac620a937e2b84489b503d3a043a7a6a6633
DanieleIacomini <danieleiacomini96@gmail.com>
2019-10-09 10:58:24 +0200

    validation form

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -1,6 +1,10 @@
+var db;
+var dbCreated = false;
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
     console.log("Device ready...");
+    var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+    
 }
 
